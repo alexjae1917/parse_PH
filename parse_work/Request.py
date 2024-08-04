ffb9f9e13d1d501611bbfc0debd20338688a61f2
alexjae1917 <alex777544@gmail.com>
2024-08-04 15:05:22 +0300

    first

---
+++ parse_work/Request.py
@@ -0,0 +1,6 @@
+class RequestEngine:
+
+    def get_request(self, url: str, params: dict | None):
+        ...
+
+
---
+++ settings.py
@@ -0,0 +1,3 @@
+HOST = 'https://platforma-ukraine.com.ua/'
+ROOT_PATH = '/nova-tehnika'
+START_PAGE = 1
